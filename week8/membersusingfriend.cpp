b52f7988dc9874d757bccb96be443177ab9a3563
Somanathan-R <103750948+Somanathan-R@users.noreply.github.com>
2022-10-28 13:29:42 +0530

    Add files via upload

--- week8/membersusingfriend.cpp
+++ week8/membersusingfriend.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
-class Base
-{   private:
+
+class Base2; //Forward declaration
+
+class Base1
+{   protected:
         int a,b;
     public:
         void input()
@@ -11,21 +14,24 @@ class Base
         {   cout<<"VALUE OF A : "<<a<<endl;
             cout<<"VALUE OF B : "<<b<<endl;
         }
-        
-        friend Base operator +(Base obj1, Base obj2);
+        friend void operator+(Base1 obj1,Base2 obj2);
 };
 
-Base operator +(Base obj1, Base obj2)
-{   Base temp;
+class Base2 : public Base1
+{ };
+
+void operator +(Base1 obj1, Base2 obj2)
+{   Base2 temp;
     temp.a = obj1.a + obj2.a;
     temp.b = obj1.b + obj2.b;
-    return temp;
+    cout<<"VALUE OF A : "<<temp.a<<endl;
+    cout<<"VALUE OF B : "<<temp.b<<endl;
 }
 
 int main()
-{   Base obj1,obj2,result;
+{   Base2 obj2;
+    Base1 obj1;
     obj1.input();
     obj2.input();
-    result = obj1 + obj2;
-    result.display();
+    obj1+obj2;
 }
